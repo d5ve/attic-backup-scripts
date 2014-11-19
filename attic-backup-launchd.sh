d0237303a58c3f5f755c0fc229825818d3d741bb
Dave Webb <wfh@dcw.co.nz>
2014-11-19 21:18:28 +1300

    DCW - Tweaked to backup my homedir, and to prune well

--- attic-backup-launchd.sh
+++ attic-backup-launchd.sh
@@ -1,18 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-REPOSITORY=username@remoteserver.com:repository.attic
+HOMEDIR=/Users/dave.webb
+REPOSITORY=$HOMEDIR/Backups/WFH.attic
+NOW=$(date '+%Y-%m-%d_%H%M%S')
 
-# Backup all of /home and /var/www except a few
-# excluded directories
+# Backup all of HOMEDIR except excluded directories
 attic create --stats                            \
-    $REPOSITORY::hostname-`date +%Y-%m-%d`      \
-    /home                                       \
-    /var/www                                    \
-    --exclude /home/*/.cache                    \
-    --exclude /home/Ben/Music/Justin\ Bieber    \
-    --exclude '*.pyc'
+    $REPOSITORY::WFH-automated-$NOW             \
+    $HOMEDIR                                    \
+    --exclude $HOMEDIR/.Trash                   \
+    --exclude $HOMEDIR/.cache                   \
+    --exclude $HOMEDIR/Applications             \
+    --exclude $HOMEDIR/Apps                     \
+    --exclude $HOMEDIR/Backups                  \
+    --exclude $HOMEDIR/Downloads                \
+    --exclude $HOMEDIR/Library/Caches           \
+    --exclude $HOMEDIR/Movies                   \
+    --exclude $HOMEDIR/Music                    \
+    --exclude $HOMEDIR/Pictures                 \
+    --exclude $HOMEDIR/Public                   \
+    --exclude '*.pyc'                           \
+    --exclude '*.swp'
 
-# Use the `prune` subcommand to maintain 7 daily, 4 weekly
-# and 6 monthly archives.
-attic prune -v $REPOSITORY --keep-daily=7 --keep-weekly=4 --keep-monthly=6
+# Use the `prune` subcommand to maintain a reasonable set of backups.
+attic prune -v $REPOSITORY --keep-hourly=48 --keep-daily=7 --keep-weekly=4 --keep-monthly=18 --keep-yearly=10
 
