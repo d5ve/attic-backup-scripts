8378b0d6fc42f91f477a5bc84f1400cd6293805e
Dave Webb <wfh@dcw.co.nz>
2014-12-08 08:32:36 +1300

    DCW - Now excludes ~/Desktop and various media filetypes

--- attic-backup-launchd.sh
+++ attic-backup-launchd.sh
@@ -14,14 +14,19 @@ $ATTIC_BIN create --stats                       \
     --exclude $HOMEDIR/Applications             \
     --exclude $HOMEDIR/Apps                     \
     --exclude $HOMEDIR/Backups                  \
+    --exclude $HOMEDIR/Desktop                  \
     --exclude $HOMEDIR/Downloads                \
     --exclude $HOMEDIR/Library/Caches           \
     --exclude $HOMEDIR/Movies                   \
     --exclude $HOMEDIR/Music                    \
     --exclude $HOMEDIR/Pictures                 \
     --exclude $HOMEDIR/Public                   \
+    --exclude '*.m4v'                           \
+    --exclude '*.mkv'                           \
+    --exclude '*.mp3'                           \
+    --exclude '*.mp4'                           \
     --exclude '*.pyc'                           \
-    --exclude '*.swp'
+    --exclude '*.swp'                           \
 
 # Use the `prune` subcommand to maintain a reasonable set of backups.
 $ATTIC_BIN prune -v $REPOSITORY --keep-hourly=48 --keep-daily=7 --keep-weekly=4 --keep-monthly=18 --keep-yearly=10
